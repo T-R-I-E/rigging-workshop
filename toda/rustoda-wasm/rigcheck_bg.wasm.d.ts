8e676dadac272dcb66d867222ab7755270f518d8
dann <gh@mrmuster.com>
2026-05-16 23:46:17 -0400

    symlinkin

--- toda/rustoda-wasm/rigcheck_bg.wasm.d.ts
+++ toda/rustoda-wasm/rigcheck_bg.wasm.d.ts
@@ -1,9 +1,1 @@
-/* tslint:disable */
-/* eslint-disable */
-export const memory: WebAssembly.Memory;
-export const check_rig: (a: number, b: number, c: number, d: number, e: number, f: number) => [number, number];
-export const __wbindgen_externrefs: WebAssembly.Table;
-export const __wbindgen_malloc: (a: number, b: number) => number;
-export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
-export const __wbindgen_free: (a: number, b: number, c: number) => void;
-export const __wbindgen_start: () => void;
+../../../rustoda/wasm/rigcheck_bg.wasm.d.ts
